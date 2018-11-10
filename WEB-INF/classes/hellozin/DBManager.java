f213e0dbff2a20373e374a32f5e78d9a9c544ee1
YoungJinAn <paul26375@gmail.com>
2018-11-10 22:12:25 +0900

    DB Manager : ArrayList import

--- WEB-INF/classes/hellozin/DBManager.java
+++ WEB-INF/classes/hellozin/DBManager.java
@@ -5,6 +5,7 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class DBManager {
 	final String JDBC_URL = "jdbc:mysql://localhost/jspdb?serverTimezone=UTC&useUnicode=true&characterEncoding=utf8";
